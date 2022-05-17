ce01a0faaa0a0d831460d18d80b066535f7e0f4b
lemb0o <lemb0o@protonmail.com>
2022-05-18 01:01:08 +0200

    Fixing links and paths

--- wp-config.php
+++ wp-config.php
@@ -21,10 +21,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mowazi' );
+define( 'DB_NAME', 'mowazi');
 
 /** MySQL database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
